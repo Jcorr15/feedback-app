113bf8495a9206cb3f4f84fa1faf6475c04ec4fc
Jovan <jovancorri1@gmail.com>
2022-07-28 18:32:18 -0400

    Added Header and FeebackList components

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'very high quality',
+  },
+  {
+    id: 2,
+    rating: 7,
+    text: 'could be better',
+  },
+  {
+    id: 3,
+    rating: -1,
+    text: 'this sucks',
+  },
+];
+
+export default FeedbackData;
