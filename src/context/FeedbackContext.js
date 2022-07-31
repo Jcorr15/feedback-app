cc075a414ba561caece89aaabc48d1e85cf2af15
Jovan <jovancorri1@gmail.com>
2022-07-31 10:59:29 -0400

    Fix editing bug

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -58,9 +58,12 @@ export const FeedbackProvider = ({ children }) => {
 
     const data = await response.json();
 
-    setFeedback(
-      feedback.map(item => (item.id === id ? { ...item, ...data } : item))
-    );
+    setFeedback(feedback.map(item => (item.id === id ? data : item)));
+
+    setFeedbackEdit({
+      item: {},
+      edit: false,
+    });
   };
 
   // Set item to be updated
